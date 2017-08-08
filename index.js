1a2ac0975a7dbea5f3c890bb0aaaac47cf388b7f
xufeng1994 <30763520+xufeng1994@users.noreply.github.com>
2017-08-08 13:48:12 +0800

    添加了注释

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require("chromedriver");
- var webdriver = require("selenium-webdriver");
-var web = new webdriver.Builder().forBrowser("chrome").build();
-web.get("http://www.baidu.com");
-web.findElement({id:"kw"}).sendKeys("xufeng");
-web.findElement({id:"su"}).click();
+ var webdriver = require("selenium-webdriver"); 
+var web = new webdriver.Builder().forBrowser("chrome").build();       //新建一个web 打开浏览器
+web.get("http://www.baidu.com");                                      //打开一个网页
+web.findElement({id:"kw"}).sendKeys("xufeng");                        //找到搜索框，键入xufeng
+web.findElement({id:"su"}).click();                                   //找到搜索框，点击
 web.get("http://www.yahoo.com");
 web.findElement({id:"uh-search-box"}).sendKeys("xufeng");
 web.findElement({id:"uh-search-button"}).click();
